a22828e867dbed5917005ea65711582f29758cd4
huihuihuihui-hue <dekisugii575@gmail.com>
2025-03-23 13:09:08 +0530

    flask app

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+happy = 40
+pleaseHappy= 50
+
+@app.route("/wish")
+def pleaseHappyRegretadd():
+    return "Hello, Jenkins"
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port="5000")
+
+
+# 0.0.0.0 ==> all traffic
+# the default port no is always 5000
